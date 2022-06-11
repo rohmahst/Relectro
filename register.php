6469477d9a4e03102d43916be940e1a05e25a4ab
Siti Rohmah <strohmahafiz@gmail.com>
2022-06-11 23:06:04 +0800

    login register

--- register.php
+++ register.php
@@ -45,19 +45,21 @@
 	<form class="form-signin">
 		<h1>Relectro</h1>
 		<h4>Toko Laptop dan Komputer</h4>
-		<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+		<h1 class="h3 mb-3 font-weight-normal">Please register</h1>
 		<label for="inputEmail" class="sr-only">Email address</label>
 		<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
 		<label for="inputPassword" class="sr-only">Password</label>
 		<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
 
 		<button class="btn btn-lg btn-primary btn-block" type="submit" style="background-color: #FFC800;">
-			Sign in
+			Sign up
 		</button>
 
+		<!-- already have an account? -->
 		<div class="mt-5 mb-3 text-muted">
-			Don't have an account? <a href="register.php">Sign up</a>
+			Already have an account? <a href="login.php">Sign in</a>
 		</div>
+
 
 		<p class="mt-5 mb-3 text-muted">&copy; 2022-now</p>
 	</form>
