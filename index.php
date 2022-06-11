5204ddcaef4d8b319e93fdd009800e318d3dd210
Siti Rohmah <strohmahafiz@gmail.com>
2022-06-11 22:29:24 +0800

    navbar

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="" />
 	<meta name="author" content="" />
-	<title>Agency - Start Bootstrap Theme</title>
+	<title>Relectro</title>
 	<!-- Favicon-->
 	<link rel="icon" type="image/x-icon" href="dist/assets/favicon.ico" />
 	<!-- Font Awesome icons (free version)-->
@@ -25,18 +25,18 @@
 	<!-- Navigation-->
 	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
 		<div class="container">
-			<a class="navbar-brand" href="#page-top"><img src="dist/assets/img/navbar-logo.svg" alt="..." /></a>
+			<!-- <a class="navbar-brand" href="#page-top"><img src="dist/assets/img/navbar-logo.svg" alt="..." /></a> -->
+			<a class="navbar-brand" href="#page-top"><h2 style="font-family: 'Brush Script MT'">Relectro</h2></a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				Menu
 				<i class="fas fa-bars ms-1"></i>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-					<li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-					<li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
-					<li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-					<li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
-					<li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+					<li class="nav-item"><a class="nav-link" href="#beranda">Beranda</a></li>
+					<li class="nav-item"><a class="nav-link" href="#produk">Produk</a></li>
+					<li class="nav-item"><a class="nav-link" href="#keranjang">Keranjang</a></li>
+					<li class="nav-item"><a class="nav-link" href="#login">Login</a></li>
 				</ul>
 			</div>
 		</div>
